cec96331d990a063e33dd5f2eaec83f6d7e5dabd
ZINAN <97530806+Zinan100@users.noreply.github.com>
2022-05-29 15:01:56 +0530

    Update caching.py

--- Alita/utils/caching.py
+++ Alita/utils/caching.py
@@ -2,18 +2,12 @@ from threading import RLock
 from time import perf_counter, time
 from typing import List
 
-from cachetools import TTLCache
 from pyrogram.types import CallbackQuery
 from pyrogram.types.messages_and_media.message import Message
 
 from Alita import LOGGER
 
 THREAD_LOCK = RLock()
-
-# admins stay cached for 30 mins
-ADMIN_CACHE = TTLCache(maxsize=512, ttl=(60 * 30), timer=perf_counter)
-# Block from refreshing admin list for 10 mins
-TEMP_ADMIN_CACHE_BLOCK = TTLCache(maxsize=512, ttl=(60 * 10), timer=perf_counter)
 
 
 async def admin_cache_reload(m: Message or CallbackQuery, status=None) -> List[int]:
