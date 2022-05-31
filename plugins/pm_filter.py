82d1bfb493dfe899c78171983a9f11452935a12d
ZINAN <97530806+Zinan100@users.noreply.github.com>
2022-05-31 19:45:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1404,6 +1404,7 @@ async def auto_filter(client, msg, spoll=False):
         btn.insert(0,
             [InlineKeyboardButton(text=f"ғɪʟᴇs: {len(files)} ", url="dupe"),
              InlineKeyboardButton(text="ᴛɪᴩ", callback_data="dupe")]
+
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
     TEMPLATE = settings['template']
     if imdb:
