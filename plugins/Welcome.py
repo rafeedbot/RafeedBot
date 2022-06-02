6a5927b9ae39dfbb94dd3c87d0effc7dfd82b2dd
ZINAN <97530806+Zinan100@users.noreply.github.com>
2022-06-02 17:27:02 +0530

    Update Welcome.py

--- plugins/Welcome.py
+++ plugins/Welcome.py
@@ -9,7 +9,7 @@ async def welcome_msg(bot, msg):
 
 @Rocky.on_message(filters.command("setwelcome"))
 async def welcome_txt(bot, msg):
-    WELCOME_MSG = await msg.reply_to_maessage.text
+    WELCOME_MSG = await msg.reply_to_message.text
     mention = msg.from_user.mention
     id = msg.from_user.id
     name = msg.from_user.first_name
