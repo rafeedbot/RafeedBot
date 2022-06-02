bbcdf2ae902dcdc6767b11f0f6d829c658e4f70c
ZINAN <97530806+Zinan100@users.noreply.github.com>
2022-06-02 17:16:22 +0530

    Create Welcome.py

---
+++ plugins/Welcome.py
@@ -0,0 +1,18 @@
+from pyrogram import filters, Client as Rocky
+from pyrogram.types import Message
+
+@Rocky.on_message(filters.new_chat_members & filters.group)
+async def welcome_msg(bot, msg)
+    await msg.reply_text(
+        text=WELCOME_MSG
+    )
+
+@Rocky.on_message(filters.command(setwelcome))
+async def welcome_txt(bot, msg)
+    WELCOME_MSG = await msg.reply_to_maessage.text
+    mention = msg.from_user.mention
+    id = msg.from_user.id
+    name = msg.from_user.first_name
+    username = msg.from_user.username
+    title = msg.from_chat.title
+    await msg.reply_text("""YOURE WELCOME MESSAGE IS : {WELCOME_MSG}
