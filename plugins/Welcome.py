13055efa9739961d6f87b90bdc2ea8f75ae4760a
ZINAN <97530806+Zinan100@users.noreply.github.com>
2022-06-02 17:35:06 +0530

    Update Welcome.py

--- plugins/Welcome.py
+++ plugins/Welcome.py
@@ -2,19 +2,19 @@ from pyrogram import filters, Client as Rocky
 from pyrogram.types import Message
 
 @Rocky.on_message(filters.new_chat_members & filters.group)
-async def welcome_msg(bot, msg):
+async def welcome_msg(bot, message):
     await msg.reply_text(
         text=f"{WELCOME_MSG}"
     )
 
 @Rocky.on_message(filters.command("setwelcome"))
-async def welcome_txt(bot, msg):
-    WELCOME_MSG = await msg.reply_to_msg.text
-    mention = msg.from_user.mention
-    id = msg.from_user.id
-    name = msg.from_user.first_name
-    username = msg.from_user.username
-    title = msg.from_chat.title
-    await msg.reply_text(
+async def welcome_txt(bot, message):
+    WELCOME_MSG = await message.reply_to_msg.text
+    mention = message.from_user.mention
+    id = message.from_user.id
+    name = message.from_user.first_name
+    username = message.from_user.username
+    title = message.from_chat.title
+    await message.reply_text(
         text=f"YOURE WELCOME MESSAGE IS : {WELCOME_MSG}"
     )
