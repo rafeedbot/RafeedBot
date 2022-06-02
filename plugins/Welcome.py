cf6cde3009f66124fe54c777122f571cfb0ffe20
ZINAN <97530806+Zinan100@users.noreply.github.com>
2022-06-02 17:53:31 +0530

    Update Welcome.py

--- plugins/Welcome.py
+++ plugins/Welcome.py
@@ -9,12 +9,14 @@ async def welcome_msg(bot, message):
 
 @Rocky.on_message(filters.command("setwelcome"))
 async def welcome_txt(bot, message):
-    WELCOME_MSG = message.reply_to_message.text
+    WELCOME_MSG = message.reply_to_message.text(chat_id, user_id)
     mention = message.from_user.mention
     id = message.from_user.id
     name = message.from_user.first_name
     username = message.from_user.username
     title = message.chat.title
+    user_id = message.reply_to_message.from_user.id
+    chat_id = message.chat.id
     await message.reply_text(
         text=f"YOURE WELCOME MESSAGE IS : {WELCOME_MSG}"
     )
