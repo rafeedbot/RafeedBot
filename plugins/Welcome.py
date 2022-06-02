ecf5941fa388e26c4061cdbee12f4f5f3cab3af1
ZINAN <97530806+Zinan100@users.noreply.github.com>
2022-06-02 17:20:02 +0530

    Update Welcome.py

--- plugins/Welcome.py
+++ plugins/Welcome.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message
 @Rocky.on_message(filters.new_chat_members & filters.group)
 async def welcome_msg(bot, msg)
     await msg.reply_text(
-        text=WELCOME_MSG
+        text=f"{WELCOME_MSG}"
     )
 
 @Rocky.on_message(filters.command(setwelcome))
@@ -15,4 +15,6 @@ async def welcome_txt(bot, msg)
     name = msg.from_user.first_name
     username = msg.from_user.username
     title = msg.from_chat.title
-    await msg.reply_text("""YOURE WELCOME MESSAGE IS : {WELCOME_MSG}
+    await msg.reply_text(
+        text=f"YOURE WELCOME MESSAGE IS : {WELCOME_MSG}"
+    )
