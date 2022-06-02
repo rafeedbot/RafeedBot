339fe11444e4f113ea454463303bdb36acc5eec3
ZINAN <97530806+Zinan100@users.noreply.github.com>
2022-06-02 18:31:21 +0530

    Update Welcome.py

--- plugins/Welcome.py
+++ plugins/Welcome.py
@@ -12,12 +12,8 @@ async def welcome_txt(bot, message):
     title = message.chat.title
     user_id = message.reply_to_message.from_user.id
     chat_id = message.chat.id
+    await message.new_chat_members
     await message.reply_text(
         text=f"YOURE WELCOME MESSAGE IS : {WELCOME_MSG}"
     )
-    expect
-@Rocky(filters.new_chat_members & filters.group)
-async def welcome_msg(bot, message):
-    await message.reply_text(
-        text=f"{WELCOME_MSG}"
-    )
+    
