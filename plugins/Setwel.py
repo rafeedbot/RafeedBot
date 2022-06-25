6deb26a9998fdf31d2a7d0ecd79a463d9d889054
ZINAN <97530806+Zinan100@users.noreply.github.com>
2022-06-25 15:28:52 +0530

    Update Setwel.py

--- plugins/Setwel.py
+++ plugins/Setwel.py
@@ -1,10 +1,10 @@
 from pyrogram import Client as Mr_bots, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 
 
 @Mr_bots.on_message(filters.command('setwelcome'))
-async def welcome(bot: Messages, id: chat_id):
+async def welcome(bot: Message, id: chat_id):
     welcome = message.reply_to_message.text
     await id.save(welcome)
     await bot.reply_text(
