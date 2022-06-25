3840e467ccea72705d9fd3d943e869a42a425c0f
ZINAN <97530806+Zinan100@users.noreply.github.com>
2022-06-25 15:20:09 +0530

    Update Setwel.py

--- plugins/Setwel.py
+++ plugins/Setwel.py
@@ -5,13 +5,14 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Mr_bots.on_message(filters.command('setwelcome'))
 async def welcome(bot: Messages, id: chat_id):
-        if reply nothing:
+    welcome = message.reply_to_message.text
+    await id.save(welcome)
+    await bot.reply_text(
+        text=f"youre welcome message saved {title} is {welcome}"
+    )
+      except: 
             await bot reply_text(
                 text="nothing provided for welcome"
             )
-        else:
-            welcome = message.reply_to_message.text
-            await id.save(welcome)
-            await bot.reply_text(
-                text=f"youre welcome message saved {title} is {welcome}"
-            )
+        
+            
